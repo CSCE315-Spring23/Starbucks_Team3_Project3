ebd63833e7da989c4291353bf06b86b7e71fe33e
YvanTonthat <yvan.tonthat@tamu.edu>
2023-04-04 02:18:53 -0500

    Added buttons with labels.

---
+++ frontend/src/pages/manager_view.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+function Button(props) {
+    return (
+        <button onClick={props.onClick}>
+            {props.label}
+        </button>
+    );
+}
+
+function Column(props) {
+    return (
+        <div>
+            {props.buttons.map(button => (
+                <Button
+                    key={button.id}
+                    onClick={button.onClick}
+                    label={button.label}
+                />
+            ))}
+        </div>
+    );
+}
+
+function manager_view() {
+    function handleClick() {
+        alert('button has been clicked.')
+    }
+
+    const buttonCol = [
+        {
+            id: 1,
+            onClick: () => handleClick(),
+            label: "Switch to Server View"
+        },
+        {
+            id: 2,
+            onClick: () => handleClick(),
+            label: "Show Inventory"
+        },
+        {
+            id: 3,
+            onClick: () => handleClick(),
+            label: "Show Low Stock Inventory"
+        },
+        {
+            id: 4,
+            onClick: () => handleClick(),
+            label: "Show Menu Item List"
+        },
+        {
+            id: 5,
+            onClick: () => handleClick(),
+            label: "Show Sales"
+        },
+        {
+            id: 6,
+            onClick: () => handleClick(),
+            label: "Show Transactions"
+        },
+        {
+            id: 7,
+            onClick: () => handleClick(),
+            label: "Show Employee List"
+        },
+        {
+            id: 8,
+            onClick: () => handleClick(),
+            label: "Show X/Z Reports"
+        }
+    ]
+
+    return (
+        <div>
+            <Column buttonColumn={buttonCol} />
+        </div>
+    );
+}
