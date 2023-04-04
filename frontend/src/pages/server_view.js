7d8f86935f44fe50230308a3a753f9b76f82c84c
YvanTonthat <yvan.tonthat@tamu.edu>
2023-04-04 05:57:25 -0500

    Added buttons for the different categories of menu items.

---
+++ frontend/src/pages/server_view.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+function Button(props) {
+    return (
+        <button onClick={props.onClick}>
+            {props.label}
+        </button>
+    );
+}
+
+function Row(props) {
+    return (
+        <div style={{ display: 'flex' }}>
+            {props.buttons.map(button => (
+                <Button
+                    key={button.id}
+                    onClick={button.onClick}
+                    label={button.label}
+                />
+            ))}
+        </div>
+    );
+}
+
+function server_view() {
+    function handleClick() {
+        alert("This button has been clicked.")
+    }
+
+    const buttonRow = [
+        {
+            id: "coffee-view",
+            onClick: () => handleClick(),
+            label: "Coffee View"
+        },
+        {
+            id: "espresso-view",
+            onClick: () => handleClick(),
+            label: "Espresso View"
+        },
+        {
+            id: "blended-view",
+            onClick: () => handleClick(),
+            label: "Blended View"
+        },
+        {
+            id: "tea-view",
+            onClick: () => handleClick(),
+            label: "Tea View"
+        },
+        {
+            id: "coffee-alt-view",
+            onClick: () => handleClick(),
+            label: "Coffee Alternatives View"
+        },
+        {
+            id: "food-view",
+            onClick: () => handleClick(),
+            label: "Food View"
+        }
+    ]
+
+    return (
+        <div>
+            <Row buttonRow={buttonRow} />
+        </div>
+    );
+}
