43337e9d3599167901e8a509b3d64c9056150798
David Liu <david_liu@tamu.edu>
2023-04-04 11:27:18 -0500

    Completed the simple orderlist api page

--- backend/api/orderlist.py
+++ backend/api/orderlist.py
@@ -1,15 +1,37 @@
-from flask import request
+from flask import request, jsonify
 from app import app
 
 import models.orderlist as ol
 
+@app.route("/orderlist", methods=["GET"])
+def createOrderlist():
+    return ol.getNewTransactionID()
 
-@app.post("/orderlist")
-def postOrderlist():
-    data = request.get_json()
-    orderlist = ol.Orderlist()
-    orderlist.addOrderlist(data.get("orderlist"))
-    orderlist.addDiscounts(data.get("discounts"))
-    orderlist.processOrderlist()
-    
+@app.route("/orderlist/<int:id>", methods=["GET", "PUT", "DELETE"])
+def editOrderlist(id):
+    if request.method == "GET":
+        return ol.getJSON(id)
+    elif request.method == "PUT":
+        ol.processOrderlist(id)
+    else:
+        ol.deleteOrderlist(id)
 
+@app.route("/orderlist/<int:id>/add-item", methods=["PUT"])
+def editItem(id):
+    orderlist = request.form["orderlist"]
+    ol.addItem(id, orderlist)
+
+@app.route("/orderlist/<int:id>/remove-item", methods=["PUT"])
+def editItem(id):
+    orderlist = request.form["orderlist"]
+    ol.removeItem(id, orderlist)
+
+@app.route("/orderlist/<int:id>/add-discount", methods=["PUT"])
+def editItem(id):
+    discounts = request.form["discounts"]
+    ol.addDiscount(id, discounts)
+
+@app.route("/orderlist/<int:id>/remove-discount", methods=["PUT"])
+def editItem(id):
+    discounts = request.form["discounts"]
+    ol.removeDiscount(id, discounts)
