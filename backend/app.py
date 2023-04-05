b4212e3376c2bdc432469ed3c0752e92208bf441
Devon <devonkel4@tamu.edu>
2023-04-04 20:39:54 -0500

    Undid changes

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,10 @@
-from flask import Flask, session, jsonify, request
+from flask import Flask
 from flask_cors import CORS #comment this on deployment
 # from api.orderlist import *
 # from api.management import *
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
-
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SECRET_KEY'] = 'your-secret-key-here'
-
-if 'order' not in session:
-    session['order'] = []
 
 @app.route("/")
 def main():
@@ -21,24 +15,6 @@ def serve():
     return [{"name": "serve"},
            {"name": "paul"},
            {"name": "lucas"}]
-
-
-@app.route('/api/order/add', methods=['POST'])
-def add_item():
-    item = request.json
-    session['order'].append(item)
-    return jsonify({'message': 'Item added to order'})
-
-
-@app.route('/api/order/remove', methods=['POST'])
-def remove_item():
-    item = request.json
-    session['order'].remove(item)
-    return jsonify({'message': 'Item removed from order'})
-
-@app.route('/api/order/get', methods=['GET'])
-def get_order_list():
-    return jsonify(session['order'])
 
 import api.management as _
 import api.orderlist as _
