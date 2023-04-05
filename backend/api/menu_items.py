5fdf9b7be46ac336ddf3a053b70fda8b5aed3ef4
David Liu <david_liu@tamu.edu>
2023-04-04 20:06:20 -0500

    display only ten items, also have categories

--- backend/api/menu_items.py
+++ backend/api/menu_items.py
@@ -7,6 +7,6 @@ import private_tools.menu_items as mi
 @app.route("/menu-items", methods=["GET"])
 def getMenuItems():
     menuItemsTable = mi.getAllMenuItems()
-    menuItems = [row[1] for row in menuItemsTable]
-    menuItemsJSON = [{"name": name} for name in menuItems]
-    return menuItemsJSON, 200
+    menuItems = [{"name": row[1], "category": row[3]} for row in menuItemsTable]
+    menuItemsJSON = [{"name": name} for name in menuItems[:10]]
+    return menuItems[20:30], 200
