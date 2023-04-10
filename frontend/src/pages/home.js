917b4483aaf4f8bb12167070ae58eb8eb81ea133
David Liu <david_liu@tamu.edu>
2023-04-10 08:55:58 -0500

    added some css changes

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -6,8 +6,10 @@ function Home() {
     const navigate = useNavigate();
     return (
         <div className="App">
-            <h2>Welcome to Starbucks!</h2>
-            <button className="nav-button" onClick={() => navigate("/server")}>Server Page</button>
+            <div className="home-page-frame">
+                <h2>Welcome to Starbucks!</h2>
+                <button className="nav-button" onClick={() => navigate("/server")}>Server Page</button>
+            </div>
         </div>
     );
 }
