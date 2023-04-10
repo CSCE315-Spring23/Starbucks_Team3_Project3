9554de4b2c7b756fe711d6abf1e6549a8336ea11
Sam Hirvilampi <samuli.hirvilampi@tamu.edu>
2023-04-10 07:47:16 -0500

    OAuth initial push

--- backend/api/login.py
+++ backend/api/login.py
@@ -1,48 +1,81 @@
-from flask import request, jsonify
+import os
+import pathlib
+import requests
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 from app import app
-import hashlib
-#import psycopg2;
+app.secret_key = "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"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+GOOGLE_CLIENT_ID = "784838590397-09qh9626eog7edfqacf6obnkjhdiplam.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://localhost/callback"
+)
 
-@app.route('/login', methods=['POST'])
-def attemptLogin():
-    #Grab employeeID and pin from request form data
-    id = int(request.form['employeeID'])
-    pin = int(request.form['employeePIN'])
-    md5 = hashlib.md5()
-    hashedPIN = md5.update(pin.encode('utf-8')).hexdigest()
-
-    # Connect to database
-    #TODO: Create conn
-    try:
-        # Create a cursor object
-        cursor = conn.cursor()
-        # Running a query looks for a entered id in the employee table
-        sqlStmt = "SELECT * FROM employees WHERE employee_id=%s"
-        # Execute query with id parameter
-        cursor.execute(sqlStmt, (id,))
-        result = cursor.fetchone()
-
-        # If statement checking if the ID and pins are a match
-        if result['employeeID'] == id and result['employee_pin'] == hashedPIN:
-            if result['access_mgmt']:
-                isManager = True
-                print("Log-in Success! Access Permission: Manager")
-            else:
-                isManager = False
-                print("Log-in Success! Access Permission: Barista")
-
-            #if isManager:
-                #TODO: Switch To Manager View
-
-            #else:
-                #TODO: Switch to Server View
-
-        # Invalid login
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
         else:
-            print("Could not find user. Check username or PIN.")
+            return function()
 
-    except Exception as e:
-        print(e)
-        raise e
-    #finally:
-        #close connection ex: conn.close()
+    return wrapper
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/")
+def index():
+    return "Login <a href='/login'><button>Login</button></a>"
+
+
+@app.route("/protected_area")
+@login_is_required
+def protected_area():
+    return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
