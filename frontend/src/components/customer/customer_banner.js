1158f0f07a61127d4d516f46d02aa314e0ae6410
Sam Hirvilampi <samuli.hirvilampi@tamu.edu>
2023-04-25 18:12:03 -0500

    Added "Change Lanuage" integration with the widget

--- frontend/src/components/customer/customer_banner.js
+++ frontend/src/components/customer/customer_banner.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import image from "../../pictures/starbucks-logo-png-25.png"
 
@@ -6,16 +6,29 @@ import "../../css/customer.css"
 
 function CustomerBanner({ setSection }) {
   const navigate = useNavigate();
+  const [showTranslate, setShowTranslate] = useState(false);
 
-  
+  {/* Code below will toggle the google translate widget on click*/}
+  const handleTranslateClick = () => {
+    const wrapper = document.getElementById('google_translate_wrapper');
+    if (wrapper) {
+      wrapper.style.display = wrapper.style.display === 'none' ? 'block' : 'none';
+    }
+  };
 
   return (
   <div className='banner'>
     <div className="starbucks-logo" style={{backgroundImage:`url(${image}`}}></div>
     <div className='weather-time'> Weather and Time </div>
-    <button className='banner-button'> Change Language </button>
+    <button className='banner-button' onClick={handleTranslateClick}> Change Language </button>
     <button className='banner-button'> Show Menu Board </button>
     <button className='banner-button' onClick={() => navigate("/login")}> Login Button </button>
+    {/* Code below makes the translation widget visible when Change Language button is clicked */}
+    {showTranslate && (
+        <div className="google-translate" id="google_translate_wrapper">
+          <div className="google-translate" id="google_translate_element"></div>
+        </div>
+      )}
   </div>
   )
 }
