f22d7a0100a9103621ee9e817a5868ba12449f16
jmaxm-tamu <jmaxm@tamu.edu>
2023-04-26 08:49:42 -0500

    CSS and test menu board

--- frontend/src/pages/menu_board_view.js
+++ frontend/src/pages/menu_board_view.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import "../css/full_menu.css"
+
 function MenuBoard() {
   return (
-    <div>MenuBoard</div>
+
+      <div className='background'>
+        <div>
+          <h1>Menu Board</h1>
+        </div>
+        <div>
+        <body>
+          <div className='full-menu'>
+            <p>Hot mocha</p>
+            <p>Hot mocha</p>
+            <p>Hot mocha</p>
+            <p>Hot mochaaaaaaaaaaa</p>
+          </div>
+        </body>
+        </div>
+      </div>
   )
 }
 
