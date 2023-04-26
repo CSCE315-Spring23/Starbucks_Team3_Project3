e0fed34eff5994bb1b34f0a801ac5ae463a2aad2
Sam Hirvilampi <samuli.hirvilampi@tamu.edu>
2023-04-25 20:27:07 -0500

    Worked on integrating weather API. Not done.

--- frontend/src/components/customer/customer_banner.js
+++ frontend/src/components/customer/customer_banner.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import image from "../../pictures/starbucks-logo-png-25.png"
 
@@ -8,28 +8,43 @@ function CustomerBanner({ setSection }) {
   const navigate = useNavigate();
   const [showTranslate, setShowTranslate] = useState(false);
 
-  {/* Code below will toggle the google translate widget on click*/}
-  const handleTranslateClick = () => {
+  {/* Code below will toggle the google translate widget on click*/ }
+  const HandleTranslateClick = () => {
     const wrapper = document.getElementById('google_translate_wrapper');
     if (wrapper) {
       wrapper.style.display = wrapper.style.display === 'none' ? 'block' : 'none';
     }
   };
 
+    // const [value, setValue] = useState([]);
+    // useEffect(() => {
+    //   apiValues()
+    // }, [])
+    // const apiValues = async () => {
+    //   const resp = await fetch('http://localhost:5000/weather/77840')
+    //   setValue(await resp.json())
+    // }
+  
   return (
-  <div className='banner'>
-    <div className="starbucks-logo" style={{backgroundImage:`url(${image}`}}></div>
-    <div className='weather-time'> Weather and Time </div>
-    <button className='banner-button' onClick={handleTranslateClick}> Change Language </button>
-    <button className='banner-button'> Show Menu Board </button>
-    <button className='banner-button' onClick={() => navigate("/login")}> Login Button </button>
-    {/* Code below makes the translation widget visible when Change Language button is clicked */}
-    {showTranslate && (
+    <div className='banner'>
+      <div className="starbucks-logo" style={{ backgroundImage: `url(${image}` }}></div>
+      <div className='weather-time'> Weather and Time 
+      {/* {value.map((data) => {
+        return(
+          <li className="weather-time" key={data.name}> {data.temp}</li>
+        )
+      })} */}
+      </div>
+      <button className='banner-button' onClick={HandleTranslateClick}> Change Language </button>
+      <button className='banner-button'> Show Menu Board </button>
+      <button className='banner-button' onClick={() => navigate("/login")}> Login Button </button>
+      {/* Code below makes the translation widget visible when Change Language button is clicked */}
+      {showTranslate && (
         <div className="google-translate" id="google_translate_wrapper">
           <div className="google-translate" id="google_translate_element"></div>
         </div>
       )}
-  </div>
+    </div>
   )
 }
 
